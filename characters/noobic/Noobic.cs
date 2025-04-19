5fb7998c74ca2fb01bf5080f8a0bf5de7089e8a8
Yuri <uendeligz@gmail.com>
2025-04-19 18:52:35 +0300

    Add basic movement

--- characters/noobic/Noobic.cs
+++ characters/noobic/Noobic.cs
@@ -1,6 +1,31 @@
 using Godot;
-using System;
 
 public partial class Noobic : CharacterBody2D
 {
+    public const int MoveSpeed = 100;
+
+    private AnimatedSprite2D sprite;
+
+    private Vector2 currentInputDirection = Vector2.Zero;
+
+    public override void _Ready()
+    {
+        sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+        sprite.Play("run_down");
+    }
+
+    public override void _Process(double delta)
+    {
+        
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        currentInputDirection = Input.GetVector("left", "right", "up", "down");
+
+        Velocity = currentInputDirection * MoveSpeed;
+
+        MoveAndSlide();
+    }
 }
